c96f3410d4573d89dbd3e6f00de3d2edd64ff897
Alexander Vladimirov <alexandervladimirov1902@gmail.com>
2016-08-08 14:44:09 +0300

    Implimented Runnable.

--- counter/src/main/java/com/clouway/counter/Main.java
+++ counter/src/main/java/com/clouway/counter/Main.java
@@ -12,7 +12,6 @@ import java.io.InputStreamReader;
  */
 public class Main {
 
-
     public static void main(String[] args) {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
 
