4b16f017e1dc5b203f9396e93b878caf0ff31407
Alexander Vladimirov <alexandervladimirov1902@gmail.com>
2016-08-08 09:34:46 +0300

    Remove counter non interrupted test.

--- counter/src/test/java/com/clouway/counter/CounterTest.java
+++ counter/src/test/java/com/clouway/counter/CounterTest.java
@@ -16,26 +16,10 @@ import static org.junit.Assert.*;
  *         <alexandervladimirov1902@gmail.com>
  */
 public class CounterTest {
-    @Test
-    public void notInterruptedThread() throws Exception {
-        Thread thread = new Thread(new Counter(10));
-        ThreadMonitor threadMonitor = new ThreadMonitor(killSymbol);
-        ByteArrayOutputStream systemOutString = new ByteArrayOutputStream();
-        PrintStream ps = new PrintStream(systemOutString);
-        PrintStream old = System.out;
-
-        System.setOut(ps);
-        long threadStartTime= System.currentTimeMillis();
-        thread.start();
-        System.out.println(threadMonitor.kill(thread,threadStartTime,11000));
-        System.out.flush();
-
-        assertThat(systemOutString.toString(), is(equalTo("Thread-2 10\nnull\n")));
-        System.setOut(old);
-    }
 
     @Test
     public void interruptedThread(){
+        String killSymbol = "kill";
         Thread thread = new Thread(new Counter(10));
         ThreadMonitor threadMonitor = new ThreadMonitor(killSymbol);
         ByteArrayOutputStream systemOutString = new ByteArrayOutputStream();
@@ -43,12 +27,12 @@ public class CounterTest {
         PrintStream old = System.out;
 
         System.setOut(ps);
-        long threadStartTime = System.currentTimeMillis();
+
         thread.start();
-        System.out.println(threadMonitor.kill(thread,threadStartTime,1000));
+        System.out.println(threadMonitor.kill(thread,killSymbol));
         System.out.flush();
 
-        assertThat(systemOutString.toString(), is(equalTo("Thread-5 0\nThread-5 interrupted!\n")));
+        assertThat(systemOutString.toString(), is(equalTo("Thread-1 0\nThread-1 interrupted!\n")));
         System.setOut(old);
     }
 }
